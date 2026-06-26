d66c1a781f3edb8b6f07c68998a8561e854cc0d0
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-06-27 07:44:08 +0800

    remove useless style

--- src/components/style.rs
+++ src/components/style.rs
@@ -1,14 +1,14 @@
 pub struct Tree;
 impl Tree {
-    pub const STEM: &'static str = "font-bold text-fuchsia-300 font-mono";
+    pub const STEM: &'static str = "font-bold text-fuchsia-300";
     pub const LEAF: &'static str =
-        "pl-5 text-fuchsia-300 hover:text-black hover:bg-fuchsia-300 font-mono";
+        "pl-5 text-fuchsia-300 hover:text-black hover:bg-fuchsia-300";
 }
 
 pub struct Doc;
 impl Doc {
-    pub const TITLE: &'static str = "text-2xl font-bold font-bold text-fuchsia-300 font-mono";
-    pub const HEADING: &'static str = "text-1xl font-bold font-bold text-fuchsia-300 font-mono";
+    pub const TITLE: &'static str = "text-2xl font-bold font-bold text-fuchsia-300";
+    pub const HEADING: &'static str = "text-1xl font-bold font-bold text-fuchsia-300";
 
     pub const LINK: &'static str =
         "hover:text-black hover:bg-fuchsia-300 underline text-fuchsia-300";
@@ -27,7 +27,7 @@ impl Table {
 }
 
 impl TableGrey {
-    pub const TABLE: &'static str = "border-collapse border border-white   w-full h-full";
+    pub const TABLE: &'static str = "border-collapse border border-white w-full h-full";
     pub const LINK: &'static str = "hover:text-black hover:bg-white-300 underline";
     pub const HEAD: &'static str = "";
     pub const HEAD_CELL: &'static str = "text-left";
