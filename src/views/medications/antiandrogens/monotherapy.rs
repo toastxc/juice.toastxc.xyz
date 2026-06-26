ffcde91bc70a1a10ad7ee0e7188b000933350e6f
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-06-27 07:23:22 +0800

    write monotherapy doc

--- src/views/medications/antiandrogens/monotherapy.rs
+++ src/views/medications/antiandrogens/monotherapy.rs
@@ -4,10 +4,22 @@ use dioxus::prelude::*;
 #[component]
 pub fn Monotherapy() -> Element {
     rsx! {
-        p { class: "{Doc::TITLE} title" }
+           div { class: "my-1 border-2 bg-red-100 p-1 text-red-900 dark:bg-red-800 dark:text-red-50 dark:shadow-white",
+            "monotherapy often requires exceeding recommended doses of estrogen"
 
-        p { class: "{Doc::HEADING} ", "Heading" }
+        }
 
-        p { "text" }
+        p { class: "{Doc::TITLE} title-monotherapy" }
+
+        p { class: "{Doc::HEADING} ", "What is monotherapy?" }
+
+        p { "Monotherapy uses estradiol as the only antiandrogenic drug, this requires \
+        a very high dose of estrogen. The practise is not very well studied." }
+
+
+             p { class: "{Doc::HEADING} ", "Suitable Drugs" }
+
+        p { "Many forms of estrogen either lack to potency needed to achieve monotherapy or are too dangerous. \
+        Patches (you'll likely need to use multiple), injections (high dose), and implants are the most likely to work." }
     }
 }
