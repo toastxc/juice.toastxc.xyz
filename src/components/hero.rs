19b39f9a8da8d6d96701d6cd3dcd4f9ef6c5c745
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-06-27 08:29:33 +0800

    remake main page

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -6,9 +6,22 @@ use dioxus::prelude::*;
 pub fn Hero() -> Element {
     rsx! {
 
-        p { "here you'll find a basic guide on HRT medications (and what to do with them)" }
+        pre { class: "text-xs font-extrabold leading-[1.1]",
+            r#"   ___       _
+  |_  |     (_)
+    | |_   _ _  ___ ___
+    | | | | | |/ __/ _ \
+/\__/ / |_| | | (_|  __/_
+\____/ \__,_|_|\___\___(_)"#
+        }
+        br {}
+        p { "HRT Information and advice, for Aussies with informed consent." }
 
         tree::Index {}
+
+        footer { class: "text-sm text-gray-300 absolute bottom-0 m-2",
+            "If you find any errors or have suggestions, please forward to juice@toastxc.xyz"
+        }
 
     }
 }
